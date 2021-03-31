0c3453cccc630ba068894d29d5304598dd187d13
Andrew <andrew.c.wong5@gmail.com>
2021-03-31 00:14:44 -0400

    update

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
