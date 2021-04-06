eb1cc019b43043ac84dd6dd454fb6a99bec3ddd9
Andrew <andrew.c.wong5@gmail.com>
2021-04-05 23:40:29 -0400

    added cards.js

---
+++ src/components/Cards.js
@@ -0,0 +1,113 @@
+import React from 'react';
+
+const Cards = () =>
+// Creative Product Card UI Design
+<div class="row">
+  <div class="column">
+
+    <div class="container">
+      <div class="card">
+        <div class="imgBx">
+          <img src="images/sneaker.png" id="sneaker">
+        </div>
+      <div class="contentBx">
+        <h2>Nike Shoes</h2>
+        <div class="size">
+          <h3>Size :</h3>
+          <span>7</span>
+          <span>8</span>
+          <span>9</span>
+          <span>10</span>
+        </div>
+          <div class="color">
+            <h3>Color :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <a href="#">Buy Now</a>
+        </div> <!--contentBx-->
+      </div> <!--card-->
+    </div> <!--container-->
+  </div> <!--//column-->
+  <div class="column">
+    <div class="container">
+      <div class="card">
+        <div class="imgBx">
+          <img src="images/sneaker.png" id="sneaker">
+        </div>
+        <div class="contentBx">
+          <h2>Nike Shoes</h2>
+          <div class="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+            <div class="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div> //color
+            <a href="#">Buy Now</a>
+        </div> <!--contentBx-->
+      </div> <!--card-->
+    </div> <!--container-->
+  </div> <!--column-->
+  <div class="column">
+    <div class="container">
+      <div class="card">
+        <div class="imgBx">
+          <img src="images/sneaker.png" id="sneaker">
+        </div>
+        <div class="contentBx">
+          <h2>Nike Shoes</h2>
+          <div class="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+            <div class="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div> //color
+            <a href="#">Buy Now</a>
+        </div> <!--contentBx-->
+      </div> <!--card-->
+    </div> <!--container-->
+  </div> <!--column-->
+  <div class="column">
+    <div class="container">
+      <div class="card">
+        <div class="imgBx">
+          <img src="images/sneaker.png" id="sneaker">
+        </div>
+        <div class="contentBx">
+          <h2>Nike Shoes</h2>
+          <div class="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+            <div class="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div> //color
+            <a href="#">Buy Now</a>
+        </div> <!--contentBx-->
+      </div> <!--card-->
+    </div> <!--container-->
+  </div> <!--column-->
+</div> <!--row--></div>
+
+export default Cards
